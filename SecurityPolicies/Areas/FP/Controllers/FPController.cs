2f5c9c91903190ce2887cd45d984910676ffe62f
Phil Pursglove <philpursglove@users.noreply.github.com>
2018-07-19 22:25:00 +0100

    FP demo

---
+++ SecurityPolicies/Areas/FP/Controllers/FPController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SecurityPolicies.Areas.FP.Controllers
+{
+    public class FPController : Controller
+    {
+        // GET: FP/FP
+        public ActionResult UseBrowserFeatures()
+        {
+            return View();
+        }
+
+        [FeaturePolicyFilter]
+        public ActionResult BlockedBrowserFeatures()
+        {
+            return View();
+        }
+    }
+
+    public class FeaturePolicyFilter : ActionFilterAttribute
+    {
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            base.OnActionExecuted(filterContext);
+
+            filterContext.HttpContext.Response.Headers.Add("Feature-Policy", "camera 'none'");
+        }
+    }
+}
---
+++ SecurityPolicies/Areas/FP/FPAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace SecurityPolicies.Areas.FP
+{
+    public class FPAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "FP";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "FP_default",
+                "FP/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
