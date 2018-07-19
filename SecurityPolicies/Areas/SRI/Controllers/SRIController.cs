0f3a3a100e56418a3c309f5ac7b9a4dcac94cedf
Phil Pursglove <philpursglove@users.noreply.github.com>
2018-07-19 21:20:52 +0100

    SRI demos

---
+++ SecurityPolicies/Areas/SRI/Controllers/SRIController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SecurityPolicies.Areas.SRI.Controllers
+{
+    public class SRIController : Controller
+    {
+        // GET: SRI/SRI
+        public ActionResult WorkingLink()
+        {
+            return View();
+        }
+
+        public ActionResult BrokenLink()
+        {
+            return View();
+        }
+
+        public ActionResult BrokenLinkWithFallback()
+        {
+            return View();
+        }
+    }
+}
---
+++ SecurityPolicies/Areas/SRI/SRIAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace SecurityPolicies.Areas.SRI
+{
+    public class SRIAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "SRI";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "SRI_default",
+                "SRI/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
