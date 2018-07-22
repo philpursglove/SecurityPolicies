1da81dd2c85681ccdaff9c0a2c7d636ff14a6ad2
Phil Pursglove <philpursglove@users.noreply.github.com>
2018-07-22 21:08:16 +0100

    CSP area

---
+++ SecurityPolicies/Areas/CSP/CSPAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace SecurityPolicies.Areas.CSP
+{
+    public class CSPAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "CSP";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "CSP_default",
+                "CSP/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ SecurityPolicies/Areas/CSP/Controllers/CSPController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SecurityPolicies.Areas.CSP.Controllers
+{
+    public class CSPController : Controller
+    {
+        // GET: CSP/CSP
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
