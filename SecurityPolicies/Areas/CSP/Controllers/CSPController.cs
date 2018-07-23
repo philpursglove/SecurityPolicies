02219861fa3761a4959e8351bb37bab0d10aeb7e
Phil Pursglove <philpursglove@users.noreply.github.com>
2018-07-23 22:23:43 +0100

    CSP demos (except combining with SRI)

--- SecurityPolicies/Areas/CSP/Controllers/CSPController.cs
+++ SecurityPolicies/Areas/CSP/Controllers/CSPController.cs
@@ -9,5 +9,35 @@ namespace SecurityPolicies.Areas.CSP.Controllers
         {
             return View();
         }
+
+        [ContentSecurityPolicy("style-src 'self' https://maxcdn.bootstrapcdn.com; script-src https://maxcdn.bootstrapcdn.com", false)]
+        public ActionResult ScriptSource()
+        {
+            return View();
+        }
+
+        [ContentSecurityPolicy("style-src 'self'", false)]
+        public ActionResult StyleSource()
+        {
+            return View();
+        }
+
+        [ContentSecurityPolicy("style-src 'self' https://maxcdn.bootstrapcdn.com; script-src https://maxcdn.bootstrapcdn.com https://code.jquery.com 'self'", false)]
+        public ActionResult UnsafeInlineOff()
+        {
+            return View();
+        }
+
+        [ContentSecurityPolicy("style-src 'self' https://maxcdn.bootstrapcdn.com; script-src https://maxcdn.bootstrapcdn.com https://code.jquery.com 'self' 'unsafe-inline';", false)]
+        public ActionResult UnsafeInlineOn()
+        {
+            return View();
+        }
+
+        [ContentSecurityPolicy("", false)]
+        public ActionResult CSPwithSRI()
+        {
+            return View();
+        }
     }
 }
