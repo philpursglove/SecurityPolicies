f4799c7473ee7ea3e9f6065994b92b96b1b54bc4
Phil Pursglove <philpursglove@users.noreply.github.com>
2018-07-23 14:31:57 +0100

    Added filter for CSP

---
+++ SecurityPolicies/Areas/CSP/Controllers/ContentSecurityPolicy.cs
@@ -0,0 +1,30 @@
+﻿using System.Web.Mvc;
+
+namespace SecurityPolicies.Areas.CSP.Controllers
+{
+    public class ContentSecurityPolicy : ActionFilterAttribute
+    {
+        private readonly string Policy;
+        private readonly bool ReportOnly;
+
+        public ContentSecurityPolicy(string policy, bool reportOnly)
+        {
+            Policy = policy;
+            ReportOnly = reportOnly;
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            base.OnActionExecuted(filterContext);
+
+            if (ReportOnly)
+            {
+                filterContext.HttpContext.Response.Headers.Add("Content-Security-Policy-Report-Only", Policy);
+            }
+            else
+            {
+                filterContext.HttpContext.Response.Headers.Add("Content-Security-Policy", Policy);
+            }
+        }
+    }
+}
