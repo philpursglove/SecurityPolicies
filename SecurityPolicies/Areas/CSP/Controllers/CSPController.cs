25718a385b6c78f472b5589a4969e8a9473d0bb2
Phil Pursglove <philpursglove@users.noreply.github.com>
2018-07-23 14:39:45 +0100

    Action etc for ReportOnly

--- SecurityPolicies/Areas/CSP/Controllers/CSPController.cs
+++ SecurityPolicies/Areas/CSP/Controllers/CSPController.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace SecurityPolicies.Areas.CSP.Controllers
 {
     public class CSPController : Controller
     {
-        // GET: CSP/CSP
-        public ActionResult Index()
+        [ContentSecurityPolicy("", true)]
+        public ActionResult ReportOnly()
         {
             return View();
         }
